d313f26f7e29300a881de1fb04940cb182b0a04f
Nils Hoffmann <nils.hoffmann@isas.de>
2017-09-11 15:51:32 +0200

    Revert "Added Small Molecule Feature and Evidence sections. Added tests."
    This reverts commit 4a45592119ffb42ae5043f1844e43e142533ddfd.

--- jmztab-model/src/main/java/uk/ac/ebi/pride/jmztab/model/Section.java
+++ jmztab-model/src/main/java/uk/ac/ebi/pride/jmztab/model/Section.java
@@ -19,21 +19,16 @@ package uk.ac.ebi.pride.jmztab.model;
  * @since 31/01/13
  */
 public enum Section {
-    Comment                          ("COM", "comment",                  0),
-    Metadata                         ("MTD", "metadata",                 1),
-    Protein_Header                   ("PRH", "protein_header",           2),
-    Protein                          ("PRT", "protein",                  3),
-    Peptide_Header                   ("PEH", "peptide_header",           4),
-    Peptide                          ("PEP", "peptide",                  5),
-    PSM_Header                       ("PSH", "psm_header",               6),
-    PSM                              ("PSM", "psm",                      7),
-    Small_Molecule_Header            ("SMH", "small_molecule_header",    8),
-    Small_Molecule                   ("SML", "small_molecule",           9),
-    Small_Molecule_Feature_Header    ("SFH", "small_molecule_feature_header",   10),
-    Small_Molecule_Feature           ("SMF", "small_molecule_feature",          11),
-    Small_Molecule_Evidence_Header   ("SEH", "small_molecule_evidence_header",   12),
-    Small_Molecule_Evidence          ("SME", "small_molecule_evidence",          13);
-    
+    Comment                  ("COM", "comment",                  0),
+    Metadata                 ("MTD", "metadata",                 1),
+    Protein_Header           ("PRH", "protein_header",           2),
+    Protein                  ("PRT", "protein",                  3),
+    Peptide_Header           ("PEH", "peptide_header",           4),
+    Peptide                  ("PEP", "peptide",                  5),
+    PSM_Header               ("PSH", "psm_header",               6),
+    PSM                      ("PSM", "psm",                      7),
+    Small_Molecule_Header    ("SMH", "small_molecule_header",    8),
+    Small_Molecule           ("SML", "small_molecule",           9);
 
     private String prefix;
     private String name;
@@ -58,38 +53,43 @@ public enum Section {
     }
 
     public static Section findSection(int level) {
+        Section section;
         switch (level) {
             case 0:
-                return Comment;
+                section = Comment;
+                break;
             case 1:
-                return Metadata;
+                section = Metadata;
+                break;
             case 2:
-                return Protein_Header;
+                section = Protein_Header;
+                break;
             case 3:
-                return Protein;
+                section = Protein;
+                break;
             case 4:
-                return Peptide_Header;
+                section = Peptide_Header;
+                break;
             case 5:
-                return Peptide;
+                section = Peptide;
+                break;
             case 6:
-                return PSM_Header;
+                section = PSM_Header;
+                break;
             case 7:
-                return PSM;
+                section = PSM;
+                break;
             case 8:
-                return Small_Molecule_Header;
+                section = Small_Molecule_Header;
+                break;
             case 9:
-                return Small_Molecule;
-            case 10:
-                return Small_Molecule_Feature_Header;
-            case 11:
-                return Small_Molecule_Feature;
-            case 12:
-                return Small_Molecule_Evidence_Header;
-            case 13:
-                return Small_Molecule_Evidence;
+                section = Small_Molecule;
+                break;
             default:
-                return null;
+                section = null;
         }
+
+        return section;
     }
 
     /**
@@ -110,14 +110,14 @@ public enum Section {
      * Judge the section is protein_header, peptide_header, psm_header or small_molecule_header section.
      */
     public boolean isHeader() {
-        return this == Protein_Header || this == Peptide_Header || this == PSM_Header || this == Small_Molecule_Header || this == Small_Molecule_Feature_Header || this == Small_Molecule_Evidence_Header;
+        return this == Protein_Header || this == Peptide_Header || this == PSM_Header || this == Small_Molecule_Header;
     }
 
     /**
      * Judge the section is protein, peptide, psm or small_molecule section.
      */
     public boolean isData() {
-        return this == Protein || this == Peptide || this == PSM || this == Small_Molecule || this == Small_Molecule_Feature || this == Small_Molecule_Evidence;
+        return this == Protein || this == Peptide || this == PSM || this == Small_Molecule;
     }
 
     /**
@@ -131,28 +131,29 @@ public enum Section {
      * @see #toDataSection(Section)
      */
     public static Section toHeaderSection(Section section) {
+        Section header;
         switch (section) {
             case Peptide:
             case Peptide_Header:
-                return Section.Peptide_Header;
+                header = Section.Peptide_Header;
+                break;
             case Protein:
             case Protein_Header:
-                return Section.Protein_Header;
+                header = Section.Protein_Header;
+                break;
             case PSM:
             case PSM_Header:
-                return Section.PSM_Header;
+                header = Section.PSM_Header;
+                break;
             case Small_Molecule:
             case Small_Molecule_Header:
-                return Section.Small_Molecule_Header;
-            case Small_Molecule_Feature:
-            case Small_Molecule_Feature_Header:
-                return Section.Small_Molecule_Feature_Header;
-            case Small_Molecule_Evidence:
-            case Small_Molecule_Evidence_Header:
-                return Section.Small_Molecule_Evidence_Header;
+                header = Section.Small_Molecule_Header;
+                break;
             default:
-                return null;
+                header = null;
         }
+
+        return header;
     }
 
     /**
@@ -166,28 +167,29 @@ public enum Section {
      * @see #toHeaderSection(Section)
      */
     public static Section toDataSection(Section section) {
+        Section data;
         switch (section) {
             case Peptide:
             case Peptide_Header:
-                return Section.Peptide;
+                data = Section.Peptide;
+                break;
             case Protein:
             case Protein_Header:
-                return Section.Protein;
+                data = Section.Protein;
+                break;
             case PSM:
             case PSM_Header:
-                return Section.PSM;
+                data = Section.PSM;
+                break;
             case Small_Molecule:
             case Small_Molecule_Header:
-                return Section.Small_Molecule;
-            case Small_Molecule_Feature:
-            case Small_Molecule_Feature_Header:
-                return Section.Small_Molecule_Feature;
-            case Small_Molecule_Evidence:
-            case Small_Molecule_Evidence_Header:
-                return Section.Small_Molecule_Evidence;
+                data = Section.Small_Molecule;
+                break;
             default:
-                return null;
+                data = null;
         }
+
+        return data;
     }
 
     /**
@@ -224,14 +226,6 @@ public enum Section {
             return Small_Molecule_Header;
         } else if (key.equalsIgnoreCase(Small_Molecule.getName()) || key.equalsIgnoreCase(Small_Molecule.getPrefix())) {
             return Small_Molecule;
-        } else if (key.equalsIgnoreCase(Small_Molecule_Feature_Header.getName()) || key.equalsIgnoreCase(Small_Molecule_Feature_Header.getPrefix())) {
-            return Small_Molecule_Feature_Header;
-        } else if (key.equalsIgnoreCase(Small_Molecule_Feature.getName()) || key.equalsIgnoreCase(Small_Molecule_Feature.getPrefix())) {
-            return Small_Molecule_Feature;
-        } else if (key.equalsIgnoreCase(Small_Molecule_Evidence_Header.getName()) || key.equalsIgnoreCase(Small_Molecule_Evidence_Header.getPrefix())) {
-            return Small_Molecule_Evidence_Header;
-        } else if (key.equalsIgnoreCase(Small_Molecule_Evidence.getName()) || key.equalsIgnoreCase(Small_Molecule_Evidence.getPrefix())) {
-            return Small_Molecule_Evidence;
         } else {
             return null;
         }
